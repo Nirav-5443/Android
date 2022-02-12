07e14cd2bb672979be2bcf30fdef136e751eb809
Nirav-5443 <nrvvekaria367@gmail.com>
2022-02-12 14:12:18 +0530

    Intent added

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.gitproject
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        intent  = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse("http://www.javatpoint.com"))
+        startActivity(intent)
         var btn : Button = findViewById(R.id.btn1)
         btn.setOnClickListener {
             Toast.makeText(applicationContext, "Hello to Android", Toast.LENGTH_LONG).show()
