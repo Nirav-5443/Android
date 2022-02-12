fa41e978e9ad11b49310ac1120a96f4863924d20
Nirav-5443 <nrvvekaria367@gmail.com>
2022-02-12 13:55:02 +0530

    Second Commit

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btn : Button = findViewById(R.id.btn1)
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello to Android", Toast.LENGTH_LONG).show()
+
+        }
     }
 }
